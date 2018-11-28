ef5e9454fba908fa7a48b434a6157ff880a22d23
lewisnyman <hello@lewisnyman.co.uk>
2018-11-28 11:39:57 +0700

    Hello world page

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+router.get('/data-listing', function (req, res) {
+    res.render('data_listing', {'message' : 'Hello world'});
+});
 
 module.exports = router
