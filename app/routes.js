98f7f5fad256746e06b4335d3212988cf21077a5
lewisnyman <hello@lewisnyman.co.uk>
2018-11-29 12:30:24 +0700

    Added new routes for edit, out of date, and update pages

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,16 @@ router.get('/data-listing', function (req, res) {
     res.render('data_listing', {'message' : 'Hello world'});
 });
 
+app.get('/data-listing/:serviceId/edit', function (req, res) {
+    res.render('service_edit', {'serviceId' : req.params.serviceId});
+})
+
+app.get('/data-listing/:serviceId/out-of-date', function (req, res) {
+    res.render('service_out_of_date', {'serviceId' : req.params.serviceId});
+})
+
+app.get('/data-listing/:serviceId/update', function (req, res) {
+    res.render('service_update', {'serviceId' : req.params.serviceId});
+})
+
 module.exports = router
