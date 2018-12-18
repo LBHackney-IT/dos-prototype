a3472e2d8785ce57ffd99e4a8d98038685ebf909
lewisnyman <hello@lewisnyman.co.uk>
2018-12-18 16:05:59 +0000

    Change all routes to all just in case

--- app/routes.js
+++ app/routes.js
@@ -7,31 +7,31 @@ router.all('/data-listing', function (req, res) {
     res.render('data_listing');
 });
 
-router.get('/data-listing/:serviceId/edit', function (req, res) {
+router.all('/data-listing/:serviceId/edit', function (req, res) {
     res.render('service_edit', {'serviceId' : req.params.serviceId, 'categories' : categories.results});
 })
 
-router.get('/data-listing/:serviceId/out-of-date', function (req, res) {
+router.all('/data-listing/:serviceId/out-of-date', function (req, res) {
     res.render('service_out_of_date', {'serviceId' : req.params.serviceId});
 })
 
-router.get('/data-listing/:serviceId/update', function (req, res) {
+router.all('/data-listing/:serviceId/update', function (req, res) {
     res.render('service_update', {'serviceId' : req.params.serviceId});
 })
 
-router.get('/data-listing/:serviceId/submission', function (req, res) {
+router.all('/data-listing/:serviceId/submission', function (req, res) {
     res.render('service_submission', {'serviceId' : req.params.serviceId, 'categories' : categories.results});
 })
 
-router.get('/service-provider-actions', function (req, res) {
+router.all('/service-provider-actions', function (req, res) {
     res.render('service_provider_actions');
 })
 
-router.get('/service-provider-actions/:serviceId/out-of-date', function (req, res) {
+router.all('/service-provider-actions/:serviceId/out-of-date', function (req, res) {
     res.render('service_out_of_date_provider', {'serviceId' : req.params.serviceId});
 })
 
-router.get('/service-provider-actions/:serviceId/edit', function (req, res) {
+router.all('/service-provider-actions/:serviceId/edit', function (req, res) {
     res.render('service_edit_provider', {'serviceId' : req.params.serviceId});
 })
 
